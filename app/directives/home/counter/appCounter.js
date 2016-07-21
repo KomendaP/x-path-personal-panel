a89f75da0080d3ba7e426faa3b62ec105e049752
Komenda Pavlo <komenda.pavlo@gmail.com>
2016-07-21 21:35:27 +0300

    [pesonal-panel][js/html] SVG norm

--- app/directives/home/counter/appCounter.js
+++ app/directives/home/counter/appCounter.js
@@ -5,19 +5,6 @@ export default ngModule => {
   "use strict";
 
   ngModule.directive('appCounter', function () {
-    let getCordsForAngle = ( X, Y, radius, angleDegr) => {
-      let angleRadians = (angleDegr - 180) * Math.PI / 180.0;
-      return {
-        x: parseInt(X + radius * Math.cos(angleRadians)),
-        y: parseInt(Y + radius * Math.sin(angleRadians))
-      }
-    };
-
-    let getArcForAngle = function (startAng, endAng, radius) {
-      let startPoint = getCordsForAngle(50, 50, radius, startAng);
-      let endPoint = getCordsForAngle(50, 50, radius, endAng);
-      return ["M", startPoint.x, startPoint.y, "A", radius, radius, 0, 0, 1, endPoint.x, endPoint.y].join(' ')
-    };
 
     let ctrl = function ($scope, DataSvc) {
       DataSvc
@@ -25,12 +12,13 @@ export default ngModule => {
         .then(data => {
           $scope.counters = data.home.counters;
         });
-      $scope.background = getArcForAngle(0, 180, 40);
     };
 
     return {
       restrict: 'E',
       template: tmpl,
+      scope: {},
+      transclude: true,
       controller: ctrl
     }
   });
